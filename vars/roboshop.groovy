2f0747c8f67c58e7f1f45583d5919ba4253f0fd6
Manmohan506 <manmohan@zelarsoft.com>
2021-06-05 19:49:48 +0530

    JENKINS

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,9 +1,19 @@
-def call(String COMPONENT) {
+def call(Map params = [:]) {
+    def args = [
+        NEXUS_IP              : '172.31.13.99',
+    ]
+    args << params
+
     pipeline {
-    agent any
+    agent {
+        label "${args.SLAVE_LABEL}"
+    }
 
     environment {
-        COMPONENT = COMPONENT
+        COMPONENT    =   "${args.COMPONENT}"
+        NEXUS_IP     =   "${args.NEXUS_IP}"
+        PROJECT_NAME =   "${args.PROJECT_NAME}"
+        SLAVE_LABEL  =   "${args.SLAVE_LABEL}"
     }
 
     stages {
@@ -21,7 +31,7 @@ def call(String COMPONENT) {
         stage('Upload Artifacts') {
           steps {
              sh '''
-               curl -f -v -u admin:DevOps321 --upload-file frontend.zip http://172.31.13.99:8081/repository/frontend-1/frontend.zip
+               curl -f -v -u admin:DevOps321 --upload-file frontend.zip http://${NEXUS_IP}:8081/repository/frontend-1/frontend.zip
              '''
     }
 }
