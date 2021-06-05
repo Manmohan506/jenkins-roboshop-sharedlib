b8b3216e88cae9b37e000a931326c7056b14fdaa
Manmohan506 <manmohan@zelarsoft.com>
2021-06-05 17:59:11 +0530

    jen

---
+++ seed.groovy
@@ -0,0 +1,131 @@
+folder('CI-Pipelines') {
+  displayName('CI Pipelines')
+  description('CI Pipelines')
+ }
+
+
+ pipelineJob("CI-Pipelines/frontend-1-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/frontend-1.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("CI-Pipelines/catalogue-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/catalogue.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("CI-Pipelines/cart-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/cart.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("CI-Pipelines/user-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/user.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("CI-Pipelines/payment-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/payment.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("CI-Pipelines/shipping-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Manmohan506/shipping.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
---
+++ vars/nexus.groovy
@@ -0,0 +1,5 @@
+def nexus() {
+ command = "curl -f -v -u admin:DevOps321 --upload-file cart.zip http://172.31.13.99:8081/repository/cart/cart.zip"
+
+def execute_state=sh(returnStdout: true, script: command)
+}
